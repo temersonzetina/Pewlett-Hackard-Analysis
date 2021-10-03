0b42e90430db1e3454077cdd0bd8aabbe617bfc9
Anthony Emerson <anthonyemerson@Anthonys-MacBook-Air.local>
2021-10-02 19:30:49 -0700

    Adding list of anticipated retirees csv file

--- schema.sql
+++ schema.sql
@@ -1,12 +1,15 @@
 -- Delete all tables.
-DROP TABLE departments;
-DROP TABLE employees;
-
+DROP TABLE departments CASCADE;
+DROP TABLE employees CASCADE;
+DROP TABLE dept_manager CASCADE;
+DROP TABLE dept_emp CASCADE;
+DROP TABLE salaries CASCADE;
+DROP TABLE title CASCADE;
 
 -- Creating tables for PH-EmployeeDB
 CREATE TABLE departments (
-	dept_no VARCHAR(4) NOT NULL,
-	dept_name VARCHAR(40) NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	dept_name VARCHAR NOT NULL,
 	PRIMARY KEY (dept_no),
 	UNIQUE (dept_name)
 );
@@ -22,13 +25,13 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE dept_manager (
-	dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
+    PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE salaries (
@@ -48,7 +51,7 @@ CREATE TABLE dept_emp (
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-	PRIMARY KEY (dept_no)
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE title (
@@ -58,17 +61,15 @@ CREATE TABLE title (
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title, from_date)
 );
 
 SELECT * FROM departments;
-
-
-
-
-
-
-
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM title;
 
 
 
