47b452c77f002160b97ddb5091f44ffe38831285
Anthony Emerson <anthonyemerson@Anthonys-MacBook-Air.local>
2021-10-03 13:15:08 -0700

    Adding Deliverable 2 with SQL and image

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -23,3 +23,21 @@ INTO retiring_titles
 FROM retiree_titles
 GROUP BY title
 ORDER BY COUNT DESC;
+
+---------------- CHALLENGE: Deliverable 2 ----------------------
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+	e.first_name, 
+	e.last_name, 
+	e.birth_date, 
+	de.from_date, 
+	de.to_date, 
+	ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN title AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
